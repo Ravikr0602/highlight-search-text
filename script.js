ece93cf39a0b0955a234cd2a75230801eab53238
Ravi Kumar <69725593+Ravikr0602@users.noreply.github.com>
2021-08-10 16:35:23 +0530

    Search text to Highlighted by using JavaScript

---
+++ script.js
@@ -0,0 +1,11 @@
+// Characters to be escaped [.*+?^${}()|[\]\\] 
+
+function searching(){
+    let Search_text = document.getElementById("search_text").value;
+    let paragraph = document.getElementById("full_paragraph");
+    Search_text = Search_text.replace(/[.*+?^${}()|[\]\\]/g,"\\$&");
+
+    let pattern = new RegExp(`${Search_text}`,"gi");
+
+    paragraph.innerHTML = paragraph.textContent.replace(pattern, match => `<mark>${match}</mark>`)
+}
